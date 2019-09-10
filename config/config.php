a0120d6f1aeb4afe53f516b53e4df73e3a79d878
Devin44G <35251347+Devin44G@users.noreply.github.com>
2019-09-10 11:18:12 -0500

    Initial Commit

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('ROOT_URL', 'http://localhost/phpSandbox/website8/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'annabellrosegraham');
+define('DB_NAME', 'phpblog');
+
+?>
---
+++ inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>PHP Blog</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+  </head>
+
+  <body>
+  <?php include('navbar.php'); ?>
