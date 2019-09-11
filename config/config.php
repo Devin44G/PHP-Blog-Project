ed07cefb0e8221b6e5f089efd81e6fe5cd7d270e
Devin44G <35251347+Devin44G@users.noreply.github.com>
2019-09-11 00:19:28 -0500

    Delete config.php

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('ROOT_URL', 'http://localhost/phpSandbox/website8/');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'annabellrosegraham');
-define('DB_NAME', 'phpblog');
-
-?>
